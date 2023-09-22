463cb33268b70bffbd91a41b4c0a95639539589c
PCov3r <pcov3r@gmail.com>
2023-09-22 18:12:16 +0100

    Update ikm.m comment

--- ikm.m
+++ ikm.m
@@ -1,3 +1,6 @@
+%% This function is an implementation of the inverse kinematics solution
+%% It returns theta1 theta2 and theta3 using the robot position x y and orientation alpha
+
 function th = ikm(param,x,y,alpha)
 % kinematic parameters
 K = param(1);
@@ -26,27 +29,3 @@ thm = 2*atan((-c-sqrt(c.^2+b.^2-a.^2)) ./ (a-b));
 th = reshape([thp;thm], size(thp,1), []);
 
 end
-
-% cph = xA-l1*cos([thp;thm])-R*cos(alpha+(4*i-3)*pi/6)+x;
-% sph = yA-l1*sin([thp;thm])-R*sin(alpha+(4*i-3)*pi/6)+y;
-% ph = atan(sph(1,:),cph(1,:));
-
-% cph1 = xA-l1*cos([thp(1);thm(1)])-R*cos(alpha+pi/6)+x;
-% sph1 = yA-l1*sin([thp(1);thm(1)])-R*sin(alpha+pi/6)+y;
-% cph2 = -xA-l1*cos([thp(2);thm(2)])-R*cos(alpha+5*pi/6)+x;
-% sph2 = yA-l1.*sin([thp(2);thm(2)])-R*sin(alpha+5*pi/6)+y;
-% cph3 = -(l1.*cos([thp(3);thm(3)])+R*cos(alpha-pi/2)-x);
-% sph3 = -(K+l1.*sin([thp(3);thm(3)])+R*sin(alpha-pi/2)-y);
-% 
-% ph1 = atan2(sph1,cph1);
-% ph2 = atan2(sph2,cph2);
-% ph3 = atan2(sph3,cph3);
-
-% ph = [ph1,ph2,ph3];
-
-%%% Solve for psi
-% psi1 = pi-ph1+pi/6+alpha;
-% psi2 = pi-ph2+5*pi/6+alpha;
-% psi3 = pi-ph3+pi/2+alpha;
-
-% psi = [psi1,psi2,psi3];
