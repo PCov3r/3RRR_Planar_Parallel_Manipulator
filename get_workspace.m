bd1406699fed37361bd2025661c0a29724bd4379
PCov3r <pcov3r@gmail.com>
2023-09-22 18:08:28 +0100

    Update get_workspace.m

--- get_workspace.m
+++ get_workspace.m
@@ -21,10 +21,10 @@ circle32 = polycircle(OA3+C3P,abs(L1-L2));
 % get circles associated with 1st kinematic chain (rotation of +120°)
 circle11 = polycircle(R1*(OA3+C3P),L1+L2);
 circle12 = polycircle(R1*(OA3+C3P),abs(L1-L2));
-% get circles associated with 2nd kinematic chain (rotation of -120°°
+% get circles associated with 2nd kinematic chain (rotation of -120°)
 circle21 = polycircle(R2*(OA3+C3P),L1+L2);
 circle22 = polycircle(R2*(OA3+C3P),abs(L1-L2));
-% (polycircle is a custom function, please refer to GitHub)
+% (polycircle is a custom function to obtain a polyshape circle: https://www.mathworks.com/help/matlab/ref/polyshape.html)
 
 % get intersection of exterior circles (external boundaries)
 polyout = intersect([circle11,circle21,circle31]);
@@ -33,43 +33,3 @@ polyout = subtract(polyout,circle12);
 polyout = subtract(polyout,circle22);
 total_wrk = subtract(polyout,circle32);
 end
-
-
-% xA=K*cos(pi/6);
-% yA=K*sin(pi/6);
-% 
-% b=R*cos(pi/6);
-% c=R*sin(pi/6);
-% 
-% figure
-% hold on
-% circle1 = subtract(circle1,circle2);
-% circle1 = subtract(circle1,circle4);
-% circle1 = subtract(circle1,circle6);
-% plot(circle1)
-% circle3 = subtract(circle3,circle2);
-% circle3 = subtract(circle3,circle4);
-% circle3 = subtract(circle3,circle6);
-% plot(circle3)
-% circle5 = subtract(circle5,circle2);
-% circle5 = subtract(circle5,circle4);
-% circle5 = subtract(circle5,circle6);
-% plot(circle5)
-% plot(-xA,-yA,'xk')
-% plot(xA,-yA,'xk')
-% plot(0,K,'xk')
-% text(-xA-40,-yA-10,'A1')
-% text(xA+20,-yA-10,'A2')
-% text(0,K+20,'A3')
-% 
-% plot(-xA+R*cos(pi/6),-yA+R*sin(pi/6),'xk')
-% plot(xA-R*cos(pi/6),-yA+R*sin(pi/6),'xk')
-% plot(0,K-R,'xk')
-% text(-xA+30,-yA+70,'A1+C1P')
-% text(xA-100,-yA+70,'A2+C2P')
-% text(-30,K-100,'A3+C3P')
-% hold off
-% xlabel("x (mm)")
-% ylabel("y (mm)")
-% axis equal
-% title('Real Workspace')
